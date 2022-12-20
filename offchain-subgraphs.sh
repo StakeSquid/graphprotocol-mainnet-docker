d78196b96b0af54df5dbcd7bc34ab9a7f6ae7dc8
Trader_Payne <baboialex95@gmail.com>
2022-12-20 12:49:28 +0200

    Merge MIPs changes (#18)
    * prepare for mips release
    
    * fix poifier
    
    * remove basic auth envs
    
    * add support for ethereum & gnosis
    
    * fix
    
    * make alt2Dockerfile default
    
    * default to one chain, let users decide if they want more
    
    * add changes to env
    
    * add .env to gitignore
    
    * split compose file

--- offchain-subgraphs.sh
+++ offchain-subgraphs.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
 
-echo -e "\e[1;32m How many subgraphs do you want to offchain sync? \e[0m"
+rm offchain-subgraphs
+
+mainnet_networks=$(http -b post "https://api.thegraph.com/subgraphs/name/graphprotocol/graph-network-mainnet" query="{networks {id}}" | jq -r .data.networks[].id)# > mainnet_networks
+testnet_networks=$(http -b post "https://api.thegraph.com/subgraphs/name/graphprotocol/graph-network-goerli" query="{networks {id}}" | jq -r .data.networks[].id)# > testnet_networks
+
+repeat=1
+
+
+while [ $repeat -gt 0 ]; do
+echo -e "\e[1;32m Which graph indexer environment do you want to query? \e[0m"
+
+select environment in "mainnet" "goerli"; do
+    echo
+    echo -e "\e[1;32m Which network do you want to sync subgraphs from? \e[0m"
+    case $environment in
+        mainnet ) select network in ${mainnet_networks}; do break;done;break;;
+        goerli ) select network in ${testnet_networks}; do break;done;break;;
+    esac
+done
+echo
+echo -e "\e[1;32m How many subgraphs do you want to offchain sync from the available ${network} subgraphs on the ${environment} environment? \e[0m"
 
 read subsnumber
 
-http  -b post https://api.thegraph.com/subgraphs/name/graphprotocol/graph-network-mainnet query="{subgraphDeployments(first:$subsnumber, orderBy:signalledTokens, orderDirection:desc, where:{signalledTokens_gt:0, deniedAt:0}) {ipfsHash}}" | jq -r .data.subgraphDeployments[].ipfsHash | sed -z 's/\n/,/g;s/,$/\n/' | tee offchain-subgraphs &> /dev/null &
+http  -b post https://api.thegraph.com/subgraphs/name/graphprotocol/graph-network-${environment} query="{subgraphDeployments(first:$subsnumber, orderBy:signalledTokens, orderDirection:desc, where:{signalledTokens_gt:0, deniedAt:0, network:\"$network\"}) {ipfsHash}}" | jq -r .data.subgraphDeployments[].ipfsHash >> offchain-subgraphs
+
+echo -e "\e[1;32m Done 👏 \e[0m"
+echo
+
+echo -e "\e[1;32m Do you wish to add more networks to the list? \e[0m"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) break;;
+        No ) repeat=0;break;;
+    esac
+done
+done
 
 echo -e "\e[1;32m Done 👏 \e[0m"
 echo
@@ -13,7 +45,7 @@ select yn in "Yes" "No"; do
     case $yn in
         Yes ) echo -e "\e[1;32m Enter your Subgraph ID or an array of Subgraph IDs (comma separated) \e[0m"
               read list
-              sed -i "s/$/,$list/" offchain-subgraphs;break;;
+              awk -F',' '{ for( i=1; i<=NF; i++ ) print $i }' <<<"$list" >> offchain-subgraphs;break;;
         No ) break;;
     esac
 done
@@ -23,14 +55,14 @@ echo
 echo -e "\e[1;32m Should I edit the start file with the changes for you? \e[0m"
 select yn in "Yes" "No"; do
     case $yn in
-         Yes ) sed -i.bak "s/\(^INDEXER_AGENT_OFFCHAIN_SUBGRAPHS=.*\)/INDEXER_AGENT_OFFCHAIN_SUBGRAPHS=$(cat offchain-subgraphs) \\\/" start;break;;
+         Yes ) sed -i.backup "s/\(^INDEXER_AGENT_OFFCHAIN_SUBGRAPHS=.*\)/INDEXER_AGENT_OFFCHAIN_SUBGRAPHS=$(cat offchain-subgraphs | sed -z 's/\n/,/g;s/,$/\n/') \\\/" .env;break;;
          No ) exit;;
      esac
  done
  echo -e "\e[1;32m Done 👏 \e[0m"
 
 echo
-echo -e "\e[1;32m Should I restart the indexer-agent container for you so that the changes apply? \e[0m"
+echo -e "\e[1;32m Should I restart the containers for you so that the changes apply? \e[0m"
 select yn in "Yes" "No"; do
     case $yn in
          Yes ) bash start --force-recreate indexer-agent;break;;
